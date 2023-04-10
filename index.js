ae8f54e8a0fd5cd46c07bf997d673f09cd934b5b
Raraph84 <contact+github@raraph.fr>
2023-04-10 19:33:48 +0200

    Add electron for system tray

--- index.js
+++ index.js
@@ -1,60 +1,67 @@
+const { app, Tray, Menu } = require("electron");
 const { Voicemeeter, StripProperties } = require("voicemeeter-connector");
-const { speaker: audio } = require("win-audio");
+const { speaker } = require("win-audio");
 const { getConfig } = require("raraph84-lib");
 const Config = getConfig(__dirname);
 
-Voicemeeter.init().then(async (voiceMeeter) => {
+app.on("ready", async () => {
 
-    voiceMeeter.connect();
+    const tray = new Tray("icon.ico");
+    tray.setContextMenu(Menu.buildFromTemplate([{
+        label: "Fermer",
+        click: () => app.quit()
+    }]));
 
-    let oldVMVolume = voiceMeeter.getStripParameter(Config.voiceMeeterWindowsStrip, StripProperties.Gain);
-    let oldVMMute = voiceMeeter.getStripParameter(Config.voiceMeeterWindowsStrip, StripProperties.Mute);
+    const voicemeeter = await Voicemeeter.init();
+    voicemeeter.connect();
 
-    let oldWindowsVolume = audio.get();
-    let oldWindowsMute = audio.isMuted();
+    let oldVMVolume = voicemeeter.getStripParameter(Config.voiceMeeterWindowsStrip, StripProperties.Gain);
+    let oldVMMute = voicemeeter.getStripParameter(Config.voiceMeeterWindowsStrip, StripProperties.Mute);
+
+    let oldWindowsVolume = speaker.get();
+    let oldWindowsMute = speaker.isMuted();
 
     const interval = setInterval(async () => {
 
-        const newVMVolume = voiceMeeter.getStripParameter(Config.voiceMeeterWindowsStrip, StripProperties.Gain);
-        const newVMMute = voiceMeeter.getStripParameter(Config.voiceMeeterWindowsStrip, StripProperties.Mute);
+        const newVMVolume = voicemeeter.getStripParameter(Config.voiceMeeterWindowsStrip, StripProperties.Gain);
+        const newVMMute = voicemeeter.getStripParameter(Config.voiceMeeterWindowsStrip, StripProperties.Mute);
 
         if (newVMVolume !== oldVMVolume) {
             oldVMVolume = newVMVolume;
             const volume = Math.min(Math.round((newVMVolume + 60) / 60 * 100), 100);
             oldWindowsVolume = volume;
-            audio.set(volume);
+            speaker.set(volume);
         }
 
         if (newVMMute !== oldVMMute) {
             oldVMMute = newVMMute;
             const mute = newVMMute === 1;
             oldWindowsMute = mute;
-            if (mute) audio.mute();
-            else audio.unmute();
+            if (mute) speaker.mute();
+            else speaker.unmute();
         }
 
-        const newWindowsVolume = audio.get();
-        const newWindowsMute = audio.isMuted();
+        const newWindowsVolume = speaker.get();
+        const newWindowsMute = speaker.isMuted();
 
         if (newWindowsVolume !== oldWindowsVolume) {
             oldWindowsVolume = newWindowsVolume;
             const volume = Math.round(-60 + newWindowsVolume / 100 * 60);
             oldVMVolume = volume;
-            voiceMeeter.setStripParameter(Config.voiceMeeterWindowsStrip, StripProperties.Gain, volume);
+            voicemeeter.setStripParameter(Config.voiceMeeterWindowsStrip, StripProperties.Gain, volume);
         }
 
         if (newWindowsMute !== oldWindowsMute) {
             oldWindowsMute = newWindowsMute;
             const mute = newWindowsMute ? 1 : 0;
             oldVMMute = mute;
-            voiceMeeter.setStripParameter(Config.voiceMeeterWindowsStrip, StripProperties.Mute, mute);
+            voicemeeter.setStripParameter(Config.voiceMeeterWindowsStrip, StripProperties.Mute, mute);
         }
 
     }, 1000 / 33);
 
-    process.on("SIGINT", () => {
+    app.on("quit", () => {
         clearInterval(interval);
-        voiceMeeter.disconnect();
-        process.exit();
+        voicemeeter.disconnect();
     });
 });
